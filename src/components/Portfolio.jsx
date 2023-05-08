e7ed5605661322ff7b6f8a8dd581df2a8e94aa27
KedarMH <kedarmh19@gmail.com>
2023-05-08 15:09:38 +0530

    about & portfolio section overlap fixed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -52,9 +52,9 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full h-screen  text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-5">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
